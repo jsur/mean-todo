96d413b42a285cb2487a9127b064d2b958e25a04
Julius Suominen <julius.suominen@gmail.com>
2016-08-12 13:07:38 +0300

    Separated API logic

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 
 app.use('/', express.static('public')); //Here express knows to serve static files from public
-
-var router = express.Router();
-
-router.get('/todos', function(req, res) {
-	res.json({todos: []});
-});
-
-// TODO: Add POST route to create new entries
-// TODO: Add PUT route to update existing entries
-// TODO: Add DELETE route to delete entries
 
 app.use('/api', router); //'/api' is the namespace where we serve from.
 
