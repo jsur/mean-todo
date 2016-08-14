3a80736fa560c723935e8e28dd6b9fad17375a5c
Julius Suominen <julius.suominen@gmail.com>
2016-08-14 17:49:01 +0300

    POST and PUT for saving todos

--- public/scripts/todo.bundle.js
+++ public/scripts/todo.bundle.js
@@ -59,8 +59,15 @@ webpackJsonp([0],[
 	        return todo
 	      };
 	    })
-	    dataService.saveTodos(filteredTodos);
-	  }; 
+	    dataService.saveTodos(filteredTodos)
+	    .finally($scope.resetTodoState());
+	  };
+
+	  $scope.resetTodoState = function() {
+	    $scope.todos.forEach(function(todo) {
+	      todo.edited = false;
+	    });
+	  }
 	});
 
 /***/ },
@@ -89,7 +96,7 @@ webpackJsonp([0],[
 	var angular = __webpack_require__(1);
 
 	angular.module('todoListApp')
-	.service('dataService', function($http) {
+	.service('dataService', function($http, $q) {
 	  this.getTodos = function(cb) {
 	    $http.get('/api/todos').then(cb);
 	  };
@@ -99,9 +106,23 @@ webpackJsonp([0],[
 	  };
 	  
 	  this.saveTodos = function(todos) {
-	    console.log("I saved " + todos.length + " todos!");
+	    var queue = [];
+	    todos.forEach(function(todo) {
+	    	var request;
+	    	if(!todo._id) {
+	    		request = $http.post('/api/todos', todo)
+	    	} else {
+	    		request = $http.put('/api/todos/' + todo._id,  todo).then(function(result) {
+	    			todo = result.data.todo;
+	    			return todo;
+	    		})
+	    	};
+	    	queue.push(request);
+	    });
+	    return $q.all(queue).then(function(results) {
+	    	console.log("I saved " + todos.length + " todos!");
+	    });
 	  };
-	  
 	});
 
 /***/ }
